87a2f0b862098456518aaa31b40bc0ddc4a9b73a
Greta911 <grema20012000@yahoo.it>
2026-09-01 10:07:22 +0200

    Découpe du template en partials

---
+++ app/views/templates/partials/_main.php
@@ -0,0 +1,29 @@
+<section class="ftco-section ftco-degree-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 ftco-animate">
+                <?php echo $content ?>
+            </div>
+
+            <!-- .col-md-8 -->
+            <?php include '../app/views/templates/partials/_aside.php' ?>
+
+            <div class="sidebar-box ftco-animate">
+                <h3>Tag Cloud</h3>
+                <div class="tagcloud">
+                    <a href="#" class="tag-cloud-link">cat</a>
+                    <a href="#" class="tag-cloud-link">abstract</a>
+                    <a href="#" class="tag-cloud-link">people</a>
+                    <a href="#" class="tag-cloud-link">person</a>
+                    <a href="#" class="tag-cloud-link">model</a>
+                    <a href="#" class="tag-cloud-link">delicious</a>
+                    <a href="#" class="tag-cloud-link">desserts</a>
+                    <a href="#" class="tag-cloud-link">drinks</a>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+    </div>
+</section> <!-- .section -->
