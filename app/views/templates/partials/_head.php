8532f48ad391d08ab0d4267365f8827c74f189a8
Greta911 <grema20012000@yahoo.it>
2026-09-08 09:11:07 +0200

    Abstraction PUBLIC_BASE_URL

--- app/views/templates/partials/_head.php
+++ app/views/templates/partials/_head.php
@@ -3,7 +3,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <base href="http://localhost/Cours%20Web%20developer/Scripts_serveur__MVC_2026/READ-IT-2026/public/">
+    <base href="<?php echo PUBLIC_BASE_URL ?>">
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
 
     <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
